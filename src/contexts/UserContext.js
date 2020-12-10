c990af80039bcd9e3a1ec56e88c7680c6eac46da
Kaan ER <kaaner91@gmail.com>
2020-12-10 17:54:41 +0300

    new user added

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -5,34 +5,38 @@ import { v4 as uuidv4 } from "uuid";
 const UserContext = createContext(null);
 
 export const UserProvider = ({ children }) => {
-	const [filter, setFilter] = useState("");
+  const [filter, setFilter] = useState("");
 
-	const [users, setUsers] = useState([
-		{
-			id: uuidv4(),
-			name: "Burak",
-		},
-		{
-			id: uuidv4(),
-			name: "Filiz",
-		},
-		{
-			id: uuidv4(),
-			name: "Batu",
-		},
-	]);
+  const [users, setUsers] = useState([
+    {
+      id: uuidv4(),
+      name: "Burak",
+    },
+    {
+      id: uuidv4(),
+      name: "Filiz",
+    },
+    {
+      id: uuidv4(),
+      name: "Batu",
+    },
+    {
+      id: uuidv4(),
+      name: "Mahmut",
+    },
+  ]);
 
-	const values = {
-		users,
-		filteredUsers: users.filter((data) =>
-			data.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
-		),
-		setUsers,
-		filter,
-		setFilter,
-	};
+  const values = {
+    users,
+    filteredUsers: users.filter((data) =>
+      data.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+    ),
+    setUsers,
+    filter,
+    setFilter,
+  };
 
-	return <UserContext.Provider value={values}>{children}</UserContext.Provider>;
+  return <UserContext.Provider value={values}>{children}</UserContext.Provider>;
 };
 
 export default UserContext;
